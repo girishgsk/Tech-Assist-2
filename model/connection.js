7c36c92a8f59b15962a949691cb4a3757d0cd2c8
Girish Kumbhar <112197471+girishgsk@users.noreply.github.com>
2024-07-26 16:57:06 +0530

    Update connection.js

--- model/connection.js
+++ model/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "database-1.czwiqmeycd6j.eu-north-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Admin#1450",
   database: "registration",
 });
 
